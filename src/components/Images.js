660864904958bd705eb4a459a6a0d7a7941364b6
mohadeseh <mohadesegharakhanlo1377@gmail.com>
2022-12-20 23:00:17 -0800

    completed images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,8 +1,24 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const Images = () => {
+  const imagesNumber = [1 , 2 , 3, 4 , 5 ,6];
   return (
-    <div>Images</div>
+    <div className='container'>
+      <div className='image-wrap'>
+        <Grid container spacing={3} columns={12}>
+          {
+            imagesNumber.map((item) => (
+              <Grid item sx={4}>
+                <div className={`image-wrap-div-${item}`}>
+                    <div className='image'></div>
+                </div>
+              </Grid>
+            ))
+          }
+        </Grid>
+      </div>
+    </div>
   )
 }
 
